fefe7859c363d57385dbe15f4aeecf1112f06a66
Bob <crowcoder@gmail.com>
2019-03-17 17:51:55 -0400

    integrated keyvault provider with app auth

--- Program.cs
+++ Program.cs
@@ -14,6 +14,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace SecretsDemo
 {
@@ -22,95 +25,111 @@ namespace SecretsDemo
         public static void Main(string[] args)
         {
 
-             CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
 
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var builtConfig = config.Build();
+                string kvname = builtConfig.GetValue<string>("kvname");
+            
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(
+                    azureServiceTokenProvider.KeyVaultTokenCallback));
+            
+                config.AddAzureKeyVault(
+                    $"https://{kvname}.vault.azure.net/",
+                    keyVaultClient,
+                    new DefaultKeyVaultSecretManager());
+            })
             .UseStartup<Startup>();
 
         public static IWebHostBuilder ManualCreateDefaultBuilder(string[] args)
-        {
-            var builder = new WebHostBuilder();
+    {
+        var builder = new WebHostBuilder();
 
-            if (string.IsNullOrEmpty(builder.GetSetting(WebHostDefaults.ContentRootKey)))
+        if (string.IsNullOrEmpty(builder.GetSetting(WebHostDefaults.ContentRootKey)))
+        {
+            builder.UseContentRoot(Directory.GetCurrentDirectory());
+        }
+
+        if (args != null)
+        {
+            builder.UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build());
+        }
+
+        builder.ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            var env = hostingContext.HostingEnvironment;
+
+            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                  .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+            if (env.IsDevelopment())
             {
-                builder.UseContentRoot(Directory.GetCurrentDirectory());
+                var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                if (appAssembly != null)
+                {
+                    config.AddUserSecrets(appAssembly, optional: true);
+                }
             }
+
+            config.AddEnvironmentVariables();
 
             if (args != null)
             {
-                builder.UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build());
+                config.AddCommandLine(args);
             }
-
-            builder.ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                var env = hostingContext.HostingEnvironment;
-
-                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                      .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-
-                if (env.IsDevelopment())
-                {
-                    var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
-                    if (appAssembly != null)
-                    {
-                        config.AddUserSecrets(appAssembly, optional: true);
-                    }
-                }
-
-                config.AddEnvironmentVariables();
-
-                if (args != null)
-                {
-                    config.AddCommandLine(args);
-                }
-            })
-            .ConfigureLogging((hostingContext, logging) =>
-            {
-                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                logging.AddConsole();
-                logging.AddDebug();
-                logging.AddEventSourceLogger();
-            }).
-            UseDefaultServiceProvider((context, options) =>
-            {
-                options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
-            });
-
-            ConfigureWebDefaults(builder);
-
-            return builder;
-        }
-
-        static void ConfigureWebDefaults(IWebHostBuilder builder)
+        })
+        .ConfigureLogging((hostingContext, logging) =>
         {
-            builder.UseKestrel((builderContext, options) =>
-            {
-                options.Configure(builderContext.Configuration.GetSection("Kestrel"));
-            })
-            .ConfigureServices((hostingContext, services) =>
-            {
+            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+            logging.AddConsole();
+            logging.AddDebug();
+            logging.AddEventSourceLogger();
+        }).
+        UseDefaultServiceProvider((context, options) =>
+        {
+            options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+        });
+
+        ConfigureWebDefaults(builder);
+
+        return builder;
+    }
+
+    static void ConfigureWebDefaults(IWebHostBuilder builder)
+    {
+        builder.UseKestrel((builderContext, options) =>
+        {
+            options.Configure(builderContext.Configuration.GetSection("Kestrel"));
+        })
+        .ConfigureServices((hostingContext, services) =>
+        {
                 // Fallback
                 services.PostConfigure<HostFilteringOptions>(options =>
+            {
+                if (options.AllowedHosts == null || options.AllowedHosts.Count == 0)
                 {
-                    if (options.AllowedHosts == null || options.AllowedHosts.Count == 0)
-                    {
                         // "AllowedHosts": "localhost;127.0.0.1;[::1]"
                         var hosts = hostingContext.Configuration["AllowedHosts"]?.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
                         // Fall back to "*" to disable.
                         options.AllowedHosts = (hosts?.Length > 0 ? hosts : new[] { "*" });
-                    }
-                });
+                }
+            });
                 // Change notification
                 services.AddSingleton<IOptionsChangeTokenSource<HostFilteringOptions>>(
-                            new ConfigurationChangeTokenSource<HostFilteringOptions>(hostingContext.Configuration));
+                        new ConfigurationChangeTokenSource<HostFilteringOptions>(hostingContext.Configuration));
 
                 // services.AddTransient<IStartupFilter, HostFilteringStartupFilter>();
             });
-            // .UseIIS()
-            // .UseIISIntegration();
-        }
+        // .UseIIS()
+        // .UseIISIntegration();
     }
+}
 }
